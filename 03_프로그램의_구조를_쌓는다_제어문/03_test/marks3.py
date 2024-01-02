63dae5877c7791a0271d3eff31abfc9fc46ec077
OhDG <ohdonggyu@naver.com>
2024-01-02 17:18:02 +0900

    03_3_for문

---
+++ 03_프로그램의_구조를_쌓는다_제어문/03_test/marks3.py
@@ -0,0 +1,10 @@
+marks = [90, 25, 67, 45, 80]
+for number in range(len(marks)):
+    if marks[number] < 60:
+        continue
+    print("%d번 학생 축하합니다. 합격입니다." % (number + 1))
+
+result = 0
+for i in range(101):
+    result += i
+print(result)
---
+++ 03_프로그램의_구조를_쌓는다_제어문/03_test/range.py
@@ -0,0 +1,6 @@
+a = range(1, 11)
+a
+print(a)
+
+for i in a:
+    print(i)
